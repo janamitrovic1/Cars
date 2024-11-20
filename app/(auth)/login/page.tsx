5cf212e20b208729444e7bbd3a48cedc013142b1
Marinko <ognjen.marinkovicc@gmail.com>
2024-11-20 15:20:08 +0100

    validacija register i sign up (bez bekenda)

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,28 +4,31 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 import { signIn } from 'next-auth/react';
+import LoginSchema from '@/components/zodschemas/LoginSchema';
 
-export default function SignInPage(){
-    const [form, setForm] = useState<{username: string, password: string}>({
-        username: "",
-        password: ""
-    });
+export default function SignInPage() {
+  const [formData, setFormData] = useState<{ username: string; password: string }>({
+    username: '',
+    password: ''
+  });
+  const [errors, setErrors] = useState<any>({});
   const router = useRouter();
 
-  const handleSubmit = async(e: any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
     try {
+      const validate = LoginSchema.safeParse(formData);
+      if (validate.success) {
         const res = await signIn('SignIn', {
-            redirect: false,
-            username: form.username,
-            password: form.password,
+          redirect: false,
+          ...validate.data
         });
-        if (!res?.ok)
-            throw new Error(res?.error?.toString());
-        else 
-            router.replace("/");
+        if (!res?.ok) throw new Error(res?.error?.toString());
+        else router.replace('/');
+      } else 
+          setErrors(validate.error.flatten().fieldErrors);
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
   };
 
@@ -34,26 +37,34 @@ export default function SignInPage(){
       <h2 className="text-2xl font-semibold text-center mb-4">Sign In</h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
-          <label className="block text-sm font-medium">Username</label>
+          <label className={`block text-sm font-medium ${errors.username ? 'text-red-500' : ''}`}>
+            Username
+          </label>
           <input
             type="text"
             name="username"
-            className="w-full px-3 py-2 mt-1 border border-gray-300 bg-gray-50 text-gray-800 rounded-md"
-            value={form.username}
-            onChange={(e) => setForm({ ...form, username: e.target.value })}
-            required
+            className={`w-full px-3 py-2 mt-1 border ${errors.username ? 'border-red-500' : 'border-gray-300'} bg-gray-50 text-gray-800 rounded-md`}
+            value={formData.username}
+            onChange={(e) => setFormData({ ...formData, username: e.target.value })}
           />
+          {errors.username && (
+            <span className="text-red-500 text-sm">{errors.username[0]}</span>
+          )}
         </div>
         <div className="mb-4">
-          <label className="block text-sm font-medium">Password</label>
+          <label className={`block text-sm font-medium ${errors.password ? 'text-red-500' : ''}`}>
+            Password
+          </label>
           <input
             type="password"
             name="password"
-            className="w-full px-3 py-2 mt-1 border border-gray-300 bg-gray-50 text-gray-800 rounded-md"
-            value={form.password}
-            onChange={(e) => setForm({ ...form, password: e.target.value })}
-            required
+            className={`w-full px-3 py-2 mt-1 border ${errors.password ? 'border-red-500' : 'border-gray-300'} bg-gray-50 text-gray-800 rounded-md`}
+            value={formData.password}
+            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
           />
+          {errors.password && (
+            <span className="text-red-500 text-sm">{errors.password[0]}</span>
+          )}
         </div>
         <button
           type="submit"
@@ -65,9 +76,9 @@ export default function SignInPage(){
       <p className="mt-4 text-center">
         Don't have an account?{' '}
         <a href="/register" className="text-blue-600 hover:underline">
-          Sign in
+          Sign up
         </a>
       </p>
     </div>
   );
-};
+}
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -2,7 +2,9 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+
 import { signIn } from 'next-auth/react';
+import RegisterSchema from '@/components/zodschemas/RegisterSchema';
 
 const SignUpPage = () => {
   const [formData, setFormData] = useState({
@@ -13,24 +15,25 @@ const SignUpPage = () => {
     BrTelefona: '',
     password: '',
   });
-
+  const [errors, setErrors] = useState<any>({});
   const router = useRouter();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const res = await signIn('SignUp', {
-        redirect: false,
-        ime: formData.ime,
-        prezime: formData.prezime,
-        username: formData.username,
-        email: formData.email,
-        BrTelefona: formData.BrTelefona,
-        password: formData.password,
-      });
-
-      if (!res?.ok) throw new Error(res?.error?.toString());
-      else router.push('/');
+      const validate = RegisterSchema.safeParse(formData); 
+      if(validate.success){
+        const res = await signIn('SignUp', {
+          redirect: false,
+          ...validate.data
+        });
+        if (!res?.ok) 
+          throw new Error(res?.error?.toString());
+        else 
+          router.push('/');
+      }
+      else
+        setErrors(validate.error.flatten().fieldErrors);
     } catch (error) {
       console.log(error);
     }
@@ -41,70 +44,82 @@ const SignUpPage = () => {
       <h2 className="text-2xl font-semibold text-center mb-4">Sign Up</h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
-          <label className="block text-sm font-medium">Ime</label>
+          <label className={`block text-sm font-medium ${errors.ime ? 'text-red-500' : ''}`}>Name</label>
           <input
             type="text"
             name="ime"
-            className="w-full px-3 py-2 mt-1 border border-gray-300 bg-gray-50 text-gray-800 rounded-md"
+            className={`w-full px-3 py-2 mt-1 border ${errors.ime ? 'border-red-500' : 'border-gray-300'} bg-gray-50 text-gray-800 rounded-md`}
             value={formData.ime}
             onChange={(e) => setFormData({ ...formData, ime: e.target.value })}
-            required
           />
+          {errors.ime && (
+            <span className="text-red-500 text-sm">{errors.ime[0]}</span>
+          )}
         </div>
         <div className="mb-4">
-          <label className="block text-sm font-medium">Prezime</label>
+          <label className={`block text-sm font-medium ${errors.prezime ? 'text-red-500' : ''}`}>Last name</label>
           <input
             type="text"
             name="prezime"
-            className="w-full px-3 py-2 mt-1 border border-gray-300 bg-gray-50 text-gray-800 rounded-md"
+            className={`w-full px-3 py-2 mt-1 border ${errors.prezime ? 'border-red-500' : 'border-gray-300'} bg-gray-50 text-gray-800 rounded-md`}
             value={formData.prezime}
             onChange={(e) => setFormData({ ...formData, prezime: e.target.value })}
-            required
           />
+          {errors.prezime && (
+            <span className="text-red-500 text-sm">{errors.prezime[0]}</span>
+          )}
         </div>
         <div className="mb-4">
-          <label className="block text-sm font-medium">Username</label>
+          <label className={`block text-sm font-medium ${errors.username ? 'text-red-500' : ''}`}>Username</label>
           <input
             type="text"
             name="username"
-            className="w-full px-3 py-2 mt-1 border border-gray-300 bg-gray-50 text-gray-800 rounded-md"
+            className={`w-full px-3 py-2 mt-1 border ${errors.username ? 'border-red-500' : 'border-gray-300'} bg-gray-50 text-gray-800 rounded-md`}
             value={formData.username}
             onChange={(e) => setFormData({ ...formData, username: e.target.value })}
-            required
           />
+          {errors.username && (
+            <span className="text-red-500 text-sm">{errors.username[0]}</span>
+          )}
         </div>
         <div className="mb-4">
-          <label className="block text-sm font-medium">Email</label>
+          <label className={`block text-sm font-medium ${errors.email ? 'text-red-500' : ''}`}>Email</label>
           <input
             type="email"
             name="email"
-            className="w-full px-3 py-2 mt-1 border border-gray-300 bg-gray-50 text-gray-800 rounded-md"
+            className={`w-full px-3 py-2 mt-1 border ${errors.email ? 'border-red-500' : 'border-gray-300'} bg-gray-50 text-gray-800 rounded-md`}
             value={formData.email}
             onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-            required
           />
+          {errors.email && (
+            <span className="text-red-500 text-sm">{errors.email[0]}</span>
+          )}
         </div>
         <div className="mb-4">
-          <label className="block text-sm font-medium">Broj telefona</label>
+          <label className={`block text-sm font-medium ${errors.BrTelefona ? 'text-red-500' : ''}`}>Phone number</label>
           <input
             type="text"
             name="BrTelefona"
-            className="w-full px-3 py-2 mt-1 border border-gray-300 bg-gray-50 text-gray-800 rounded-md"
+            className={`w-full px-3 py-2 mt-1 border ${errors.BrTelefona ? 'border-red-500' : 'border-gray-300'} bg-gray-50 text-gray-800 rounded-md`}
             value={formData.BrTelefona}
             onChange={(e) => setFormData({ ...formData, BrTelefona: e.target.value })}
-            required
           />
+          {errors.BrTelefona && (
+            <span className="text-red-500 text-sm">{errors.BrTelefona[0]}</span>
+          )}
         </div>
         <div className="mb-4">
-          <label className="block text-sm font-medium">Password</label>
+          <label className={`block text-sm font-medium ${errors.password ? 'text-red-500' : ''}`}>Password</label>
           <input
             type="password"
             name="password"
-            className="w-full px-3 py-2 mt-1 border border-gray-300 bg-gray-50 text-gray-800 rounded-md"
+            className={`w-full px-3 py-2 mt-1 border ${errors.password ? 'border-red-500' : 'border-gray-300'} bg-gray-50 text-gray-800 rounded-md`}
             value={formData.password}
             onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-            required
           />
+          {errors.password && (
+            <span className="text-red-500 text-sm">{errors.password[0]}</span>
+          )}
         </div>
         <button
           type="submit"
