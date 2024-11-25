ea6c3092f5579c39a6fc3137d2b76ef4a1313a01
Jana Mitrovic <mitrovicjanakv@gmail.com>
2024-11-25 18:02:35 +0100

    added fonts

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,21 +1,10 @@
-import Link from "next/link"
 
 // import "@app/globals.css";
 export const Nav = ()=>{
 
     return (
-        <div className="flex flex-row justify-between p-5">
-            <div className="">
-                <ul className="flex flex-row gap-4">
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/">Products</Link></li>
-                    <li><Link href="/">About</Link></li>
-                </ul>
-            </div>
-            <div>
-                <h1>CARS</h1>
-                
-            </div>
+        <div className="">
+            nav
         </div>
     )
 }
