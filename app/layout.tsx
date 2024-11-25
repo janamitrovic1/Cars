f5d13145d3f1ee8dcbfc3d1c5e570b3db6a278e0
Jana Mitrovic <mitrovicjanakv@gmail.com>
2024-11-25 19:19:23 +0100

    dodati fontovi fr sad

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,51 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Nav } from "@/components/nav";
 import AuthProvider from "@/components/auth-provider";
 
-const workSans = localFont({
-  src:[
-    {
-    path:'./fonts/WorkSans-Black.ttf',
-    weight:'900',
-    style:'normal'},{
-      path:'./fonts/WorkSans-ExtraBold.ttf',
-      weight:'800',
-      style:'normal'
-    },{
-      path:'./fonts/WorkSans-Bold.ttf',
-      weight:'700',
-      style:'normal'
-    },{
-      path:'./fonts/WorkSans-SemiBold.ttf',
-      weight:'600',
-      style:'normal'
-    },{
-      path:'./fonts/WorkSans-Medium.ttf',
-      weight:'500',
-      style:'normal'
-    },{
-      path:'./fonts/WorkSans-Regular.ttf',
-      weight:'400',
-      style:'normal'
-    },{
-      path:'./fonts/WorkSans-Thin.ttf',
-      weight:'200',
-      style:'normal'
-    },{
-      path:'./fonts/WorkSans-ExtraLight.ttf',
-      weight:'100',
-      style:'normal'
-    },
-  ],
-  variable:'--font-work-sans'
-})
+export const workSans = localFont(
+  {
+    src :[
+      {
+        path:'./fonts/WorkSans-Black.ttf',
+        weight:'900',
+        style:'normal'
+      },{
+        path:'./fonts/WorkSans-ExtraBold.ttf',
+        weight:'800',
+        style:'normal'
+      },{
+        path:'./fonts/WorkSans-Bold.ttf',
+        weight:'700',
+        style:'normal'
+      },{
+        path:'./fonts/WorkSans-SemiBold.ttf',
+        weight:'600',
+        style:'normal'
+      },{
+        path:'./fonts/WorkSans-Medium.ttf',
+        weight:'500',
+        style:'normal'
+      },{
+        path:'./fonts/WorkSans-Regular.ttf',
+        weight:'400',
+        style:'normal'
+      },{
+        path:'./fonts/WorkSans-Thin.ttf',
+        weight:'200',
+        style:'normal'
+      },{
+        path:'./fonts/WorkSans-ExtraLight.ttf',
+        weight:'100',
+        style:'normal'
+      },
+    ],
+    variable:'--font-work-sans',
+  }
+)
 export const metadata: Metadata = {
-  title: "Home",
-  description: "",
+  title: "Cars",
+  description: "Find Car For Your Needs",
 };
 
 export default function RootLayout({
@@ -57,7 +59,6 @@ export default function RootLayout({
         className={workSans.variable}
       >
         <AuthProvider>
-          <Nav/>
           {children}
         </AuthProvider>
       </body>
