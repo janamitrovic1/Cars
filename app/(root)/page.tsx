4eb9929255693dcf159d961d7ff1704c72056be4
Jana Mitrovic <mitrovicjanakv@gmail.com>
2024-11-29 15:51:39 +0100

    zavrsen product

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,24 @@
 
 import Product from "@/components/product";
 import SearchForm from "@/components/searchform";
-
+import {ProductType} from "@/components/product";
 export default async function Home({searchParams}:{
 	searchParams:Promise<{query?:string}>
 }) {
 	const query = (await searchParams).query;
     const posts = [
 		{
-			_createdAt: new Date(),
-			views:55,
-			_id:1,
-			image: "https://images.unsplash.com/photo-1634912314704-c646c586b131?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3FDB8MHxwaG90by1wYWdlfHx8GVufDB8fHx8fA%3D%3D",
-			description: "This is a description",
-			marka:"BMW",
-			model:"We Robots",
+			id:"abcdef",
+            ime:"Audi A3",
+            model:"",
+            marka:"Audi",
+            slika:"https://images.unsplash.com/photo-1634912314704-c646c586b131?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3FDB8MHxwaG90by1wYWdlfHx8GVufDB8fHx8fA%3D%3D",
+            kategorija:"Putničko vozilo",
+            cena:3000,
+            boja:"crvena",
+            brVrata:3,
+            godiste:2024,
+            description:"mnogo dobro",
 		}
 	];
 	return (
@@ -31,7 +35,7 @@ export default async function Home({searchParams}:{
                 <ul className="mt-7 card_grid">
                 {posts?.length > 0 ? (
                     posts.map((post: ProductType,number) => (
-                    <Product  key={post?._id} post={post}/>
+                    <Product post={post}  key={post?.id}/>
                     ))
                 ) : (
                     <p className="no-results">No cars found</p>
--- components/product.tsx
+++ components/product.tsx
@@ -2,51 +2,53 @@ import Image from "next/image";
 import Link from "next/link";
 import { EyeIcon } from "lucide-react";
 import { Button } from "@headlessui/react";
-import { formatDate } from "@/lib/utils";
-
+import { Proizvod } from "@prisma/client";
+    
+export type ProductType = Proizvod;
 export default function Product ({post}:{post: ProductType}){
 
-    const {_createdAt, views,marka,model,_id,image,description } = post;
+    const {id,ime,model,slika,kategorija,marka,cena,godiste,description} = post;
   return (
     <li className='product-card group'>
 
         <div className='flex-between'>
             <p className='product-card_date'>
-                {formatDate(_createdAt)}
+                {kategorija}
             </p>
             <div className='flex gap'>
                 <EyeIcon className='size-6 text-secondary-300 group-hover:text-primary transition-all duration-500'/>
-                <span className='text-16-medium'>{views}</span>
+                <span className='text-16-medium'>0</span>
             </div>
         </div>
         
 
         <div className='flex-between mt-5 gap-5'>
             <div className='flex-1'>
-                {/* <Link href={`/user/${authorId}`}>
-                    <p className='text-16-medium line-clamp-1'>{name}</p>
-                </Link> */}
-                <Link href={`/products/${post._id}`}>
-                    <h3 className='text-26-semibold line-clamp-1'>{model}</h3>
+                <Link href={`/?query=${marka}`}>
+                    <p className='text-16-medium line-clamp-1'>{marka}</p>
                 </Link>
+                <Link href={`/api/product/${post.id}`}>
+                    <h3 className='text-26-bold line-clamp-1'>{ime}</h3>
+                </Link>
+                <h5 className="text-godiste line-clamp-1">{godiste}.god</h5>
             </div>
-            <Link href={`/user/`}>
-                <Image src="https://placehold.co/600x400" alt="placeholder" width={48} height={48} className="rounded-full"/>
-            </Link>
+            <div>
+                <p className="product-cena">{cena}$</p>
+            </div>
         </div>
 
-        <Link href={`/products/${post._id}`}>
+        <Link href={`/api/product/${post.id}`}>
             <p className='product-card_desc'>{description}</p>
             
-            <Image src={image} alt="placeholder"  className='product-card_img'width={350} height={164}/>
+            <Image src={slika} alt="placeholder"  className='product-card_img' width={350} height={164}/>
         </Link>
 
         <div className='flex-between gap-3 mt-5'>
-            <Link href={`/?query=${marka.toLowerCase()}`}>
-                <p className='text-16-medium'>{marka}</p>
+            <Link href={`/`}>
+                <p className='text-16-medium'>Demo drive</p>
             </Link>
-            <Button className='product-card_btn' asChild>
-                <Link href={`/products/${_id}`}>
+            <Button className='product-card_btn'>
+                <Link href={`/api/product/${id}`}>
                     Details
                 </Link>
             </Button>
